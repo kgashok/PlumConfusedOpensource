3f78191c8bbb05c5c467a71cfc71e43d4256962f
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2025-01-01 11:07:18 +0000

    setup of tables in backend database

--- setup.sql
+++ setup.sql
@@ -1,18 +1,1 @@
-
-CREATE TABLE IF NOT EXISTS tweets (
-    id TEXT PRIMARY KEY,
-    text TEXT NOT NULL,
-    timestamp TIMESTAMP NOT NULL,
-    url TEXT NOT NULL,
-    user_id TEXT NOT NULL,
-    screen_name TEXT NOT NULL,
-    deleted BOOLEAN DEFAULT false
-);
-
-CREATE TABLE IF NOT EXISTS searched_tweets (
-    id TEXT PRIMARY KEY,
-    text TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    author_id TEXT NOT NULL,
-    url TEXT NOT NULL
-);
+404: Not Found
